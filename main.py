9e217c6f3451b992ded4878e3fb2b41d4f04b192
MadTMan <smtoufique.000@gmail.com>
2025-07-30 15:33:18 +0600

    First Chain of thought execution

--- main.py
+++ main.py
@@ -2,15 +2,52 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
 
 load_dotenv()
 
 # Add this after load_dotenv()
-import os
-print("ANTHROPIC_API_KEY:", os.getenv("ANTHROPIC_API_KEY"))
+
+# import os
+# print("ANTHROPIC_API_KEY:", os.getenv("ANTHROPIC_API_KEY"))
 
 # llm = ChatOpenAI(model="gpt-4o", temperature=0.0)
 
+class researchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
 llm = ChatAnthropic(model="claude-opus-4-20250514", temperature=0.0)
-response = llm.invoke("What is the meaning of life?")
-print(response)
+# response = llm.invoke("What is the meaning of life?")
+# print(response)
+
+parser = PydanticOutputParser(pydantic_object=researchResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+
+agent  = create_tool_calling_agent(
+    llm=llm,
+    prompt=prompt,
+    tools=[],
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=[], verbose=True)
+raw_response = agent_executor.invoke({"query": "What is the meaning of life?"})
