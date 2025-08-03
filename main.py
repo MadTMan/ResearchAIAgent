dea0344e27cc575d63c77662339609ecb49616cc
MadTMan <smtoufique.000@gmail.com>
2025-08-04 00:15:24 +0600

    Structured Response with search feature v1

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from langchain_anthropic import ChatAnthropic
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain.agents import create_tool_calling_agent, AgentExecutor
+from tools import search_tool
+
 
 load_dotenv()
 
@@ -43,14 +45,16 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 ).partial(format_instructions=parser.get_format_instructions())
 
+tools = [search_tool]
 agent  = create_tool_calling_agent(
     llm=llm,
     prompt=prompt,
-    tools=[],
+    tools=tools,
 )
 
-agent_executor = AgentExecutor(agent=agent, tools=[], verbose=True)
-raw_response = agent_executor.invoke({"query": "What is the meaning of life?"})
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+query = input("What can I help you research today? ")
+raw_response = agent_executor.invoke({"query": query})
 
 try:
     structured_response = parser.parse(raw_response.get("output")[0]["text"])
